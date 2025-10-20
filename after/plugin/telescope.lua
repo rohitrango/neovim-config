0ef62fb9868c3f0ed279fbe96288e2e237bc6dfc
rohitrango <rohit.rango@gmail.com>
2025-10-19 22:45:16 -0400

    add airline
    Signed-off-by: rohitrango <rohit.rango@gmail.com>

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,3 +10,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- we want telescope to use the fzf module
+require('telescope').load_extension('fzf')
+
--- lua/plugins/airline.lua
+++ lua/plugins/airline.lua
@@ -1,3 +1,14 @@
 return {
-    'vim-airline/vim-airline'
+    {
+        'vim-airline/vim-airline',
+        config = function()
+            vim.g['airline#extensions#tabline#enabled'] = 1
+            vim.g['airline_powerline_fonts'] = 1
+            vim.g['airline#extensions#tabline#formatter'] = 'unique_tail'
+        end,
+    },
+    {
+        'vim-airline/vim-airline-themes',
+        lazy = true,
+    }
 }
